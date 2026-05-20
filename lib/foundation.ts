6f5b01cc738f85144295b5e0d0b173348b0ac165
cryptobro-nad <jonaskhan09@gmail.com>
2026-05-20 21:24:41 +0300

    Add foundation status helper

---
+++ lib/foundation.ts
@@ -0,0 +1,10 @@
+export function getAppStatus() {
+  return {
+    name: "Monad Swap Rank",
+    hasRealApiIntegration: false,
+    hasDatabase: false,
+    hasRedis: false,
+    hasWalletConnect: false,
+    hasLeaderboard: false
+  };
+}
